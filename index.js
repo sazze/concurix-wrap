56dcb7301f0a369fe9ecd8f3346b53da17cf2475
alexgo <alex@gounares.com>
2013-11-26 00:51:19 -0800

    fix constructor case

--- index.js
+++ index.js
@@ -103,7 +103,7 @@ module.exports = function wrap(wrapFun){
 
         var _args = arguments;
         function runOriginal() {
-          if (state.orgFun.prototype && self instanceof state.orgFun) {
+          if (state.orgFun.prototype && Object.getPrototypeOf(self) === state.orgFun.prototype) {
             if (_args.length === 0) {
               return new state.orgFun()
             }
